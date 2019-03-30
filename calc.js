320b2ddd5aadb4275da403d91ba3b376fb12fa6b
ch-vasu <ch.vasu678@gmail.com>
2019-03-30 09:18:45 +0530

    Added trigonometry functionality

--- calc.js
+++ calc.js
@@ -1,30 +1,99 @@
-var result;
-result="";
+var dispExp="";
+var originalExp="";
+disp=document.getElementById("input-data");
+basic=document.getElementById("basic");
+advanced=document.getElementById("advanced");
+bas=document.getElementById("bas");
+adv=document.getElementById("adv");
+
+//advance buttons referemces
+
+root=document.getElementById("root");
+rem=document.getElementById("rem");
+div=document.getElementById("div");
+mul=document.getElementById("mul");
+sub=document.getElementById("sub");
+add=document.getElementById("add");
+pi=document.getElementById("pi");
+
 function reset(){
-  result="";
-  document.getElementById("input-data").value=0;
-  document.getElementById("input-data").style.border="2px solid black";
-  document.getElementById("input-data").style.color="black";
+  dispExp="";
+  originalExp="";
+  adv.checked=true;
+  disp.value=0;
+  disp.style.border="2px solid black";
+  disp.style.color="black";
+  disp.style.textAlign="left";
 }
-function inputExpression(data){
-
-    result +=data;
-
-    document.getElementById("input-data").value=result;
+function expression(orig,dup){
+  dispExp +=dup;
+  originalExp +=orig;
+  disp.value=dispExp;
+  console.log(originalExp,dispExp);
 }
-
 function calculate(){
   let evalValue;
-  evalValue=document.getElementById("input-data").value;
   try{
-    evalValue=eval(evalValue);
+  evalValue=eval(originalExp);
   }
-  catch(err){
-    document.getElementById("input-data").style.border="2px solid red";
-    document.getElementById("input-data").style.color="red";
-
+  catch (err){
     evalValue="Syntax Error";
+    disp.style.borderColor="red";
+    disp.style.color="red";
+    disp.style.textAlign="center";
+    disp.value="Syntax error";
   }
-  document.getElementById("input-data").value=evalValue;
-  result=evalValue;
+  disp.value=evalValue.toFixed(4);
+}
+
+function change(){
+  if(basic.checked){
+  bas.style.color="lime";
+  adv.style.color="white";
+    add.innerHTML="+";
+    add.setAttribute("onclick","expression('+','+')");
+
+    sub.innerHTML="-";
+    sub.setAttribute("onclick","expression('-','-')");
+
+    mul.innerHTML="X";
+    mul.setAttribute("onclick","expression('*','*')");
+
+    div.innerHTML="/";
+    div.setAttribute("onclick","expression('/','/')");
+
+    rem.innerHTML="Rem";
+    rem.setAttribute("onclick","expression('%','%')");
+
+    root.innerHTML="\u221A";
+    root.setAttribute("onclick","expression('Math.sqrt(','\u221A (')");
+
+    pi.innerHTML="PI";
+    pi.setAttribute("onclick","expression('Math.PI','PI')");
+  }
+  else {
+    adv.style.color="lime";
+    bas.style.color="white";
+    add.innerHTML="sin";
+    add.setAttribute("onclick","expression('Math.sin(Math.PI/180*','sin(')");
+
+    sub.innerHTML="cos";
+    sub.setAttribute("onclick","expression('Math.cos(Math.PI/180*','cos(')");
+
+    mul.innerHTML="tan";
+    mul.setAttribute("onclick","expression('Math.tan(Math.PI/180*','tan(')");
+
+    div.innerHTML="sinh";
+    div.setAttribute("onclick","expression('Math.sinh(Math.PI/180*','sinh(')");
+
+    rem.innerHTML="cosh";
+    rem.setAttribute("onclick","expression('Math.cosh(Math.PI/180*','cosh(')");
+
+    root.innerHTML="tanh";
+    root.setAttribute("onclick","expression('Math.tanh(Math.PI/180*','tanh(')");
+
+    pi.innerHTML="\u03A0 ";
+    pi.setAttribute("onclick","expression('180','\u03A0')");
+  }
+
 }
